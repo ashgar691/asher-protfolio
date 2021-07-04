45d692ca4a98015b7a538689fe8705c404d60065
asherababa <ashgar691@gmail.com>
2021-07-04 13:01:18 +0300

    add navigation

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+const MaterialModules = [
+  MatIconModule,
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ...MaterialModules],
+  exports: [...MaterialModules]
+})
+export class MaterialModule { }
